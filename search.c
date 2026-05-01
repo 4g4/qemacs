91f7ff6fe2218d0e1c023f10b695008dcd000674
4g4 <45503963+4g4@users.noreply.github.com>
2026-05-01 15:25:56 +0800

    fix: occur command didn't clear the previous result (#104)
    The `occur` command creates an `*occur*` buffer to show the matches in the current buffer.
    If we used `occur` to search another string, then the old *occur* buffer was still there and our new search result were not shown. Killing buffer `*occur*` before creating a new one solves the problem.

--- search.c
+++ search.c
@@ -1365,8 +1365,8 @@ void do_search_string(EditState *s, const char *search_str, int mode)
         last = eb_goto_bol(s->b, offset);
     }
     if (mode == CMD_LIST_MATCHING_LINES) {
-        // XXX: should check prefix argument to clear buffer
-        b1 = qe_new_buffer(s->qs, "*occur*", BC_REUSE | BF_UTF8 | (s->b->flags & BF_STYLES));
+        do_kill_buffer(s, "*occur*", 1);
+        b1 = qe_new_buffer(s->qs, "*occur*", BF_UTF8 | (s->b->flags & BF_STYLES));
         if (!b1)
             return;
         start = b1->total_size;
